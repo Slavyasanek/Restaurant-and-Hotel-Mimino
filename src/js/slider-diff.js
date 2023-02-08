a5cb4e5fbacb3083e0ac12f4bb97c1b9a654f058
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-08 12:11:47 +0200

    add new slider-set

---
+++ src/js/slider-diff.js
@@ -0,0 +1,77 @@
+function swiperSlider() {
+    const sliderDiff = document.querySelectorAll('[data-slider="slider-diff"]');
+    if (sliderDiff) {
+        sliderDiff.forEach(slider => {
+          //
+          let arrowLeft = slider.querySelector('.swiper-button-prev');
+          let arrowRight = slider.querySelector('.swiper-button-next');
+          //
+    
+          // let pagination = slider.querySelector('.swiper-pagination');
+          swiper = new Swiper(slider.querySelector('.swiper'), {
+            speed: 1500,
+            // loop: true,
+            //centeredSlides: true,
+            centeredSlides: true,
+            // autoplay: {
+            //     delay: 3000,
+            //     disableOnInteraction: false,
+            // },
+    
+            slidesPerView: 2,
+            spaceBetween: 20,
+            // pagination: {
+            //     el: pagination,
+            //     clickable: true,
+            //     renderBullet: function (index, className) {
+            //         return '<li class="' + className + '"></li>';
+            //     },
+            // },
+            //
+            navigation: {
+              nextEl: arrowRight,
+              prevEl: arrowLeft,
+            },
+            //
+            on: {
+              transitionStart: function () {
+                let previousIndex = this.previousIndex;
+                let previousSlide =
+                  slider.getElementsByClassName('swiper-slide')[previousIndex];
+                if (previousSlide) {
+                  setTimeout(function () {
+                    previousSlide.classList.remove('is-play');
+                  }, 1000);
+                }
+              },
+              transitionEnd: function () {
+                let activeIndex = this.activeIndex;
+                let activeSlide =
+                  slider.getElementsByClassName('swiper-slide')[activeIndex];
+                activeSlide.classList.add('is-play');
+              },
+            },
+            breakpoints: {
+              // when window width is >= 0px
+              0: {
+                slidesPerView: 'auto',
+                // centeredSlides: true,
+                spaceBetween: 20,
+              },
+              // when window width is >= 1280px
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 34,
+                centeredSlides: true,
+              },
+              // when window width is >= 640px
+              // 1280: {
+              //   slidesPerView: 3,
+              //   spaceBetween: 40,
+              // },
+            },
+          });
+        });
+      }
+}
+window.addEventListener('load', swiperSlider, false);
