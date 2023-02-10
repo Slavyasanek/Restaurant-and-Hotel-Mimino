170ba62ab74c592c73cb5a006d118315c6230edb
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-10 14:14:14 +0200

    connected new slider script

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './js/slider-con';
 import './js/slider-diff';
 import './js/select-set';
 import './js/slider-hero';
+import './js/slider-room';
 import './js/imask-set';
 import './js/modals-set';
